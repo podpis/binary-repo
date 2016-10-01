78338454b45e5cfb021c690562c992126b1ab5a0
tyl0re <andreas@vogler.name>
2016-10-01 21:26:23 +0200

    Update

---
+++ update.sh
@@ -0,0 +1,5 @@
+/www/repo/binary-git/repo_aktualisieren.sh /www/repo/binary-git/Generic_x68_64
+/www/repo/binary-git/repo_aktualisieren.sh /www/repo/binary-git/OSX_64
+/www/repo/binary-git/repo_aktualisieren.sh /www/repo/binary-git/RPi
+/www/repo/binary-git/repo_aktualisieren.sh /www/repo/binary-git/RPi2
+/www/repo/binary-git/repo_aktualisieren.sh /www/repo/binary-git/WIN_32
