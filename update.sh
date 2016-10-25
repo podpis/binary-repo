5b04d723da55b1e36fca3d2c377dfc2ac35300c9
vdrtuxnet <felix.l.kamps@gmail.com>
2016-10-25 11:45:09 +0200

    update pvr.zattoo to 0.1.22

--- update.sh
+++ update.sh
@@ -4,4 +4,5 @@
 /www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/RPi2
 /www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/WIN_32
 /www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/Odroid_C2
+/www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/S905
 
