f3f094e7f7d23a7286674ca6c4ac895c27781c9e
vdrtuxnet <felix.l.kamps@gmail.com>
2017-09-10 20:57:19 +0200

    Revert "set repo offline"
    This reverts commit a4d856f6c4115da1e29913b93ea8af0d8e4b4a41.

--- offline/update.sh
+++
@@ -1,4 +0,0 @@
-/www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/Generic_x68_64
-/www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/OSX_64
-/www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/RPi2
-/www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/WIN_32
