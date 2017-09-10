a4d856f6c4115da1e29913b93ea8af0d8e4b4a41
vdrtuxnet <felix.l.kamps@gmail.com>
2017-09-10 16:34:50 +0200

    set repo offline

---
+++ offline/update.sh
@@ -0,0 +1,4 @@
+/www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/Generic_x68_64
+/www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/OSX_64
+/www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/RPi2
+/www/repo/binary-git/repo_aktualisieren.sh /data/buildnew/bin-repo/WIN_32
